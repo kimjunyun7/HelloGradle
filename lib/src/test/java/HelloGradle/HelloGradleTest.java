7fe986f7da2c49b5d552988b39aefad4af841764
kimjunyun7 <kimjunyun7@gmail.com>
2021-08-18 01:39:55 +0700

    5th

---
+++ lib/src/test/java/HelloGradle/HelloGradleTest.java
@@ -0,0 +1,12 @@
+package HelloGradle;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class HelloGradleTest {
+	@Test 
+    public void testMain() {
+        System.out.println("HelloGradle main");
+}
